f75e45637320c6f7cac40ca573b584921ad70c1f
Samantha Rose Cruz <samrosecruz@gmail.com>
2020-04-24 17:48:53 +0800

    Initial Gatsby stuff

--- src/components/header.js
+++ src/components/header.js
@@ -3,31 +3,15 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header>
+    <div className="Header">
+      <div className="HeaderGroup">
+        <Link to="/courses">Courses</Link>
+      </div>
+
     </div>
+    <h1>Logo</h1>
+
   </header>
 )
 
